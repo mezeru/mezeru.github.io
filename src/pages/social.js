29e11ac428c9b3f717531f0225b0c2d06ee1dd56
Mezeru <jermpro0@gmail.com>
2021-08-29 20:56:03 +0530

    Added icone

--- src/pages/social.js
+++ src/pages/social.js
@@ -1,6 +1,8 @@
 import {html} from 'lit-html';
+import '@fortawesome/fontawesome-free/css/all.css'
 
 const social = () => {return html `
+
 
 <section class="child" id="social">
             <div class="container">
@@ -14,12 +16,12 @@ const social = () => {return html `
                 </div>
 
                 <div  class="social-items">
-                    <a href="https://github.com/mezeru" class="gliched-text social-item">Github</a>
-                    <a href="https://www.linkedin.com/in/yash-chaudhari-9194311bb/" class="gliched-text social-item">linkedin</a>
-                    <a href="https://open.spotify.com/user/31d5ihybeudzqweelv7pc7tsjsmm" class="gliched-text social-item">Spotify</a>
-                    <a href="https://www.instagram.com/_juz_someone_/" class="gliched-text social-item">Instragram</a>
-                    <a href="https://steamcommunity.com/id/Ruizolas/" class="gliched-text social-item">Steam</a>
-                    <a href="https://www.snapchat.com/add/juz.someone" class="gliched-text social-item">Snapchat</a>
+                    <a href="https://github.com/mezeru" class="gliched-text social-item"><i class="fab fa-github"></i>Github</a>
+                    <a href="https://www.linkedin.com/in/yash-chaudhari-9194311bb/" class="gliched-text social-item"><i class="fab fa-linkedin"></i>linkedin</a>
+                    <a href="https://open.spotify.com/user/31d5ihybeudzqweelv7pc7tsjsmm" class="gliched-text social-item"><i class="fab fa-spotify"></i>Spotify</a>
+                    <a href="https://www.instagram.com/_juz_someone_/" class="gliched-text social-item"><i class="fab fa-instagram"></i>Instragram</a>
+                    <a href="https://steamcommunity.com/id/Ruizolas/" class="gliched-text social-item"><i class="fab fa-steam"></i>Steam</a>
+                    <a href="https://www.snapchat.com/add/juz.someone" class="gliched-text social-item"><i class="fab fa-snapchat-ghost"></i>Snapchat</a>
                 </div>
 
             </div>
