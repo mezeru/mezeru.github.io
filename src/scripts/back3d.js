fa0f554d6636982d3c7e516dd85d3a9ab009ea94
Yash <jermpro0@gmail.com>
2023-04-30 06:48:56 -0400

    unlimted road adde

--- src/scripts/back3d.js
+++ src/scripts/back3d.js
@@ -2,10 +2,11 @@ import { init } from 'aos';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+import road from './scene/road.jpg'
 
 //import skybox images
-import fnt from './scene/skybox/back.jpg'
-import bk from './scene/skybox/front.jpg'
+import fnt from './scene/skybox/front.jpg'
+import bk from './scene/skybox/back.jpg'
 import btm from './scene/skybox/down.jpg'
 import lt from './scene/skybox/left.jpg'
 import rgt from './scene/skybox/right.jpg'
@@ -46,18 +47,39 @@ onload = () => {
 
 window.addEventListener('resize', resize());
 
-camera.position.z = 5;
+camera.position.y = 3;
+camera.position.z = 5
+ 
 
 orbit.update();
 
-const light = new THREE.AmbientLight(0xd1d1d1);
+const light = new THREE.AmbientLight(0xd1d1d1,0.5);
 light.position.x = 0;
 light.position.y = 0;
-light.position.z = 0;
+light.position.z = 10;
 scene.add(light);
 
 
-const skyboxGeo = new THREE.BoxGeometry(1000,1000,1000);
+const planeGeo = new THREE.PlaneGeometry(50,5,10,10);
+const roadTex = new THREE.TextureLoader().load(road);
+roadTex.wrapS = THREE.RepeatWrapping;
+roadTex.repeat.x = 10;
+const planeMat = new THREE.MeshStandardMaterial({map: roadTex});
+
+const planeMesh = new THREE.Mesh(planeGeo,planeMat);
+planeMesh.rotation.x += 250;
+scene.add(planeMesh);
+
+
+// animate the texture offset to create the illusion of movement
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
+
+
+const skyboxGeo = new THREE.BoxGeometry(500,500,500);
 let matArr = [];
 let skyboxarr = [fnt,bk,tp,btm,rgt,lt];
 
@@ -74,17 +96,33 @@ const skybox = new THREE.Mesh(skyboxGeo,matArr);
 
 scene.add(skybox);
 
-const tick = () => {
-    window.requestAnimationFrame(tick);
-
-    // box.rotation.x += 0.01;
-    // box.rotation.y += 0.01;
-    // box.rotation.z += 0.01;
-    renderer.render(scene,camera);
-
+const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
+    const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
+    const cube = new THREE.Mesh( geometry, material ); 
+    cube.rotation.x += 250;
+    cube.position.y += 0.5;
+    scene.add( cube );
 
 
-}
-tick();
+    const tick = () => {
+        requestAnimationFrame(tick);
+  
+    // Move the road and the car
+        planeMesh.position.x += 0.5;
+        cube.position.x += 0.01;
+        camera.position.x += 0.01;
+        renderer.render(scene, camera);
 
-renderer.render(scene,camera);
+        if(planeMesh.position.x >= camera.position.x + 10){
+            planeMesh.position.x = camera.position.x - 10;
+        }
+
+    }
+
+    tick();
+
+
+
+
+
+
