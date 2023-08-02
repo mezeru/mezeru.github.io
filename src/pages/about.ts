b4062048cdfd4d84ba711601a96ac1c758397b19
Yash <jermpro0@gmail.com>
2023-08-02 17:41:40 -0400

    another section added

--- src/pages/about.ts
+++ src/pages/about.ts
@@ -24,19 +24,66 @@ const about = () =>
         <a href="https://drive.google.com/drive/folders/1oMvbbnov6BWOqLznrQohUEX9-0sUNLtA?usp=sharing" style="align-self: flex-start; font-size: 1.8em;margin-top:2%;" class="gliched-move gliched-text" >
         <span>Resume</span><span>Resume</span> <span>Resume</span></span>
         </a>
+
         
     </section>
+
+   
+    
+
+
 
     
     `
 
-//     <section class="child abt" id="about" style="align-items:center">
 //     <div class="scroll">
-//             <div class="chevron"></div>
-//             <div class="chevron"></div>
-//             <div class="chevron"></div>
-//     </div>
+//     <div class="chevron"></div>
+//     <div class="chevron"></div>
+//     <div class="chevron"></div>
+// </div>
+
+// <section class="child" id="about" style="align-items:normal">
+
+// <div class="skills-sec">
+
+// <div style="display: flex;align-items: center;justify-content: center">
+// <span class="gliched-move gliched-text">
+//     <a style="font-size: 2em;"><span>Skills</span><span>Skills</span> <span>Skills</span></a>
+// </span>
+
+// </div>
+
+
+
+
+
+// </div>
+
 // </section>
+
+//     <div  class="social-items">
+//     <div  class="social-items">
+//         <a href="https://github.com/mezeru" class="gliched-text social-item"><i class="fab fa-github"></i>Github</a>
+//         <a href="https://www.linkedin.com/in/yash-chaudhari-9194311bb/" class="gliched-text social-item"><i class="fab fa-linkedin"></i>linkedin</a>
+//         <a href="https://open.spotify.com/user/31d5ihybeudzqweelv7pc7tsjsmm" class="gliched-text social-item"><i class="fab fa-spotify"></i>Spotify</a>
+//         <a href="https://www.instagram.com/_juz_someone_/" class="gliched-text social-item"><i class="fab fa-instagram"></i>Instragram</a>
+//         <a href="https://steamcommunity.com/id/Ruizolas/" class="gliched-text social-item"><i class="fab fa-steam"></i>Steam</a>
+//         <a href="https://www.snapchat.com/add/juz.someone" class="gliched-text social-item"><i class="fab fa-snapchat-ghost"></i>Snapchat</a>
+//     </div>
+
+//     <div  class="social-items">
+//         <a href="https://github.com/mezeru" class="gliched-text social-item"><i class="fab fa-github"></i>Github</a>
+//         <a href="https://www.linkedin.com/in/yash-chaudhari-9194311bb/" class="gliched-text social-item"><i class="fab fa-linkedin"></i>linkedin</a>
+//         <a href="https://open.spotify.com/user/31d5ihybeudzqweelv7pc7tsjsmm" class="gliched-text social-item"><i class="fab fa-spotify"></i>Spotify</a>
+//         <a href="https://www.instagram.com/_juz_someone_/" class="gliched-text social-item"><i class="fab fa-instagram"></i>Instragram</a>
+//         <a href="https://steamcommunity.com/id/Ruizolas/" class="gliched-text social-item"><i class="fab fa-steam"></i>Steam</a>
+//         <a href="https://www.snapchat.com/add/juz.someone" class="gliched-text social-item"><i class="fab fa-snapchat-ghost"></i>Snapchat</a>
+//     </div>
+
+
+// </div>
+
+
     
 }
 
